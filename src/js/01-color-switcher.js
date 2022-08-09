d65e9fb6a551069bac091c9edb7e142eb4922d91
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-08-09 18:30:22 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,8 +3,8 @@ const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
 startButton.addEventListener('click', onChangeColor);
-stopButton.addEventListener('click', noneTimer)
-
+stopButton.addEventListener('click', noneTimer);
+let timerId = null;
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
         .toString(16)
